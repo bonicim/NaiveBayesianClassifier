71dd5985c9c3ed2f3b9c67cdb30b0be5921ee95a
Mark Bonicillo <markabonicillo@gmail.com>
2020-03-11 19:34:03 -0700

    Add Classifier and Features class

---
+++ tst/test_Featutres.py
@@ -0,0 +1,71 @@
+import pytest
+from os import path
+
+test_data_path = path.abspath(
+    path.join(path.dirname(__file__), "..", "data", "tweet_test_data_small.csv")
+)
+
+
+@pytest.fixture
+def features():
+    from src.DataProcessor import DataProcessor
+    from src.Features import Features
+
+    return Features(DataProcessor(test_data_path))
+
+
+def test_extract(features):
+    expected = {"realDonaldTrump": 0.5, "HillaryClinton": 0.5}
+    expected_vocab = {
+        "question": 1,
+        "election": 1,
+        "put": 1,
+        "plans": 1,
+        "action": 1,
+        "make": 1,
+        "life": 1,
+        "better": 1,
+        "httpstcoxreey9oicg": 1,
+        "nothing": 4,
+        "emails": 1,
+        "corrupt": 1,
+        "clinton": 1,
+        "foundation": 1,
+        "benghazi": 1,
+        "debates2016": 1,
+        "debatenight": 1,
+    }
+    expected_cond_prob = {
+        "realDonaldTrump": {
+            "nothing": 0.3,
+            "emails": 0.1,
+            "corrupt": 0.1,
+            "clinton": 0.1,
+            "foundation": 0.1,
+            "benghazi": 0.1,
+            "debates2016": 0.1,
+            "debatenight": 0.1,
+        },
+        "HillaryClinton": {
+            "question": 0.1,
+            "election": 0.1,
+            "put": 0.1,
+            "plans": 0.1,
+            "action": 0.1,
+            "make": 0.1,
+            "life": 0.1,
+            "better": 0.1,
+            "httpstcoxreey9oicg": 0.1,
+            "nothing": 0.1,
+        },
+    }
+
+    probs = features.extract_features()
+
+    prior_probs = probs[0]
+    vocab = probs[1]
+    cond_prob = probs[2]
+
+    assert prior_probs == expected
+    assert vocab == expected_vocab
+    assert cond_prob == expected_cond_prob
