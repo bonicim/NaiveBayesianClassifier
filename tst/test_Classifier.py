3975c60cad99cca393a94af300994bf6bb7c9542
Mark Bonicillo <markabonicillo@gmail.com>
2020-03-12 15:58:45 -0700

    Add test for large data set

--- tst/test_Classifier.py
+++ tst/test_Classifier.py
@@ -1,27 +1,44 @@
 import pytest
 from os import path
-
-test_data_path = path.abspath(
-    path.join(path.dirname(__file__), "..", "data", "tweet_test_data_small.csv")
-)
+from src.DataProcessor import DataProcessor
+from src.Features import Features
+from src.Classifier import Classifier
 
 
 @pytest.fixture
-def classifier():
-    from src.DataProcessor import DataProcessor
-    from src.Features import Features
-    from src.Classifier import Classifier
-
+def classifier_small():
+    test_data_path = path.abspath(
+        path.join(path.dirname(__file__), "..", "data", "tweet_test_data_small.csv")
+    )
     classifier = Classifier(Features(DataProcessor(test_data_path)))
     classifier.train()
 
     return classifier
 
 
-def test_classify(classifier):
+def test_classify_small_model(classifier_small):
     tweet = "Crooked Hillary Clinton wants to flood our country with Syrian immigrants that we know little or nothing about. The danger is massive. NO!"
 
-    result = classifier.classify(tweet)
+    result = classifier_small.classify(tweet)
+
+    print(result)
+    assert result is not None
+
+
+@pytest.fixture
+def classifier_large_data_set():
+    test_data_path = path.abspath(
+        path.join(path.dirname(__file__), "..", "data", "tweets.csv")
+    )
+    classifier = Classifier(Features(DataProcessor(test_data_path)))
+    classifier.train()
+
+    return classifier
+
+
+def test_classify_large_data_set(classifier_large_data_set):
+    tweet = "No legit reason @realDonaldTrump can't release returns while being audited, but if scared, release earlier returns no longer under audit."
+    result = classifier_large_data_set.classify(tweet)
 
     print(result)
     assert result is not None
