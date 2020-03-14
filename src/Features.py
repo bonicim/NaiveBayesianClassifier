dd7a44a81d40c8938d7ff65471234c77990d1c43
Mark Bonicillo <markabonicillo@gmail.com>
2020-03-14 16:46:29 -0700

    Rename variables

--- src/Features.py
+++ src/Features.py
@@ -4,10 +4,10 @@ import math
 class Features:
     def __init__(self, data_processor):
         self._data_processor = data_processor
-        self._documents = None
+        self._tweets = None
 
     def extract_features(self):
-        self._documents = self._data_processor.process()
+        self._tweets = self._data_processor.process()
 
         prior_prob = self._build_prior_prob()
         vocab = self._build_vocab()
@@ -15,10 +15,10 @@ class Features:
         return (prior_prob, vocab, cond_prob)
 
     def _build_prior_prob(self):
-        total_docs = len(self._documents)
+        total_docs = len(self._tweets)
         prior_prob = dict()
 
-        for author, tokens in self._documents:
+        for author, tokens in self._tweets:
             count = prior_prob.setdefault(author, 0)
             prior_prob[author] = count + 1
 
@@ -30,7 +30,7 @@ class Features:
     def _build_vocab(self):
         vocab = dict()
 
-        for _, tokens in self._documents:
+        for _, tokens in self._tweets:
             for token, freq in tokens.items():
                 count = vocab.get(token, 0)
                 vocab[token] = count + freq
@@ -41,15 +41,16 @@ class Features:
         # for each author build up their own vocab
         cond_prob = dict()
 
-        for author, tokens in self._documents:
+        # get raw count of each author's word in vocab
+        for author, tweet in self._tweets:
             author_vocab = cond_prob.setdefault(author, {})
-            for token, freq in tokens.items():
-                count = author_vocab.setdefault(token, 0)
-                author_vocab[token] = count + freq
+            for word, freq in tweet.items():
+                author_vocab[word] = author_vocab.setdefault(word, 0) + freq
 
-        for author, tokens in cond_prob.items():
-            total_words = sum(tokens.values())
-            for token, freq in tokens.items():
-                tokens[token] = freq / total_words
+        # compute prob of each author word in vocab
+        for author, tweet in cond_prob.items():
+            total_words = sum(tweet.values())
+            for word, freq in tweet.items():
+                tweet[word] = freq / total_words
 
         return cond_prob
